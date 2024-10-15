4d8c0716c29a915bab0cd638a34e6bf556050147
marble-ly <385516@naver.com>
2024-10-15 22:32:56 +0900

    하단 네비게이션바 적용 완료

---
+++ app/src/main/java/com/example/univandroidproject/ui/view/HomeFragment.kt
@@ -0,0 +1,17 @@
+package com.example.univandroidproject.ui.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.univandroidproject.R
+
+class HomeFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_home, container, false)
+        //fragment_home과 연결시켜 return해줌.
+    }
+
+}
---
+++ app/src/main/java/com/example/univandroidproject/ui/view/SearchFragment.kt
@@ -0,0 +1,18 @@
+package com.example.univandroidproject.ui.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.univandroidproject.R
+
+class SearchFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_serch, container, false)
+        //fragment_home과 연결시켜 return해줌.
+    }
+
+}
