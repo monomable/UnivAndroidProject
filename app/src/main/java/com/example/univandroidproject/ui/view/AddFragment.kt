161b1b94dbc9159def8224c6736b117bd1ea8dd4
marble-ly <385516@naver.com>
2024-10-22 05:47:43 +0900

    프레그먼트 버튼 설정1

--- app/src/main/java/com/example/univandroidproject/ui/view/AddFragment.kt
+++ app/src/main/java/com/example/univandroidproject/ui/view/AddFragment.kt
@@ -6,11 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.univandroidproject.R
+import com.example.univandroidproject.databinding.FragmentAddBinding
 
-class AddFragment : Fragment() {
+class AddFragment : Fragment(), View.OnClickListener {
+
+    private  lateinit var binding: FragmentAddBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         return inflater.inflate(R.layout.fragment_add, container, false)
         //fragment_add과 연결시켜 return해줌.
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
