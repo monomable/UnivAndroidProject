de38a195df516741874c6d33d1e87547572140ee
marble-ly <385516@naver.com>
2024-10-24 00:00:13 +0900

    임시 이미지 넣기 기능 추가

--- app/src/main/java/com/example/univandroidproject/AddActivity.kt
+++ app/src/main/java/com/example/univandroidproject/AddActivity.kt
@@ -1,17 +1,66 @@
 package com.example.univandroidproject
 
+import android.Manifest
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class AddActivity : AppCompatActivity() {
 
     lateinit var daypickbutton : Button
+    lateinit var file_img : ImageView
+    lateinit var file_img2 : ImageView
+    lateinit var file_img3 : ImageView
+
+    private val permissionList = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+    private val checkPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
+        result.forEach {
+            if(!it.value) {
+                Toast.makeText(this, "권한 동의 필요!", Toast.LENGTH_SHORT).show()
+                //finish()
+            }
+
+        }
+    }
+
+    private val readImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        file_img.setImageURI(uri)
+    }
+
+    private val readImage2 = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        file_img2.setImageURI(uri)
+    }
+
+    private val readImage3 = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        file_img3.setImageURI(uri)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_add)
+
+        checkPermission.launch(permissionList)
+
+        file_img = findViewById<ImageView>(R.id.imageView)
+        file_img2 = findViewById<ImageView>(R.id.imageView2)
+        file_img3 = findViewById<ImageView>(R.id.imageView3)
+
+        file_img.setOnClickListener {
+            readImage.launch("image/*")
+        }
+
+        file_img2.setOnClickListener {
+            readImage2.launch("image/*")
+        }
+
+        file_img3.setOnClickListener {
+            readImage3.launch("image/*")
+        }
+
+
 
         daypickbutton = findViewById<Button>(R.id.dayPicker_Button)
         daypickbutton.setOnClickListener{
--- app/src/main/java/com/example/univandroidproject/ui/view/AddFragment.kt
+++ app/src/main/java/com/example/univandroidproject/ui/view/AddFragment.kt
@@ -1,19 +1,41 @@
 package com.example.univandroidproject.ui.view
 
+import android.Manifest
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.univandroidproject.AddActivity
 import com.example.univandroidproject.R
 import com.example.univandroidproject.MainActivity
+import com.google.android.gms.maps.MapView
 import java.util.Calendar
 
 class AddFragment : Fragment(){
+
+    private lateinit var file_img : ImageView
+
+    private val permissionList = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+    private val checkPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
+        result.forEach {
+            if(!it.value) {
+                Toast.makeText(activity, "권한 동의 필요!", Toast.LENGTH_SHORT).show()
+                //finish()
+            }
+
+        }
+    }
+
+    private val readImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        file_img.setImageURI(uri)
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_add, container, false)
@@ -22,6 +44,16 @@ class AddFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        checkPermission.launch(permissionList)
+
+        val file_img = view.findViewById<ImageView>(R.id.imageView)
+
+        file_img.setOnClickListener {
+            Toast.makeText(AddActivity(), "날짜 선택 버튼 눌림", Toast.LENGTH_SHORT).show()
+            readImage.launch("image/*")
+
+        }
 
         val daypickbutton: Button = view.findViewById<Button>(R.id.dayPicker_Button)
         daypickbutton.setOnClickListener{
